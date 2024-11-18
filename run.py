c644dfaf7333183123c4e9beab7047cf9995e208
Iiya Zakharov <2146376@gmail.com>
2024-11-19 01:09:45 +0300

    Add docs for front

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from settings import settings
 from app.__init__ import app
+import subprocess
 
 if __name__ == '__main__':
+    subprocess.run(['python', 'docs\\generate_docs.py'])
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=settings.APP_PORT)
