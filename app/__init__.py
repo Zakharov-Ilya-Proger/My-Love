54ab65f9f4c406b992a9a1f0bd1eacdff8535121
Iiya Zakharov <2146376@gmail.com>
2024-11-19 14:38:47 +0300

    sosvojdi

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from app.endpoints.student import student
 from app.endpoints.teacher import teacher
 from app.endpoints.neutral import neutral
 
+
 app.include_router(main)
 app.include_router(student, prefix="/student", tags=["student"])
 app.include_router(teacher, prefix="/teacher", tags=["teacher"])
