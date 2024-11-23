95892f78c833701a0d96f227296b1cf35af4ab1d
Iiya Zakharov <2146376@gmail.com>
2024-11-23 20:27:40 +0300

    Fix routing

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ from app.endpoints.student import student
 from app.endpoints.teacher import teacher
 from app.endpoints.neutral import neutral
 
-app.include_router(main, tags=["Main"])
-app.include_router(student, prefix="/student", tags=["Student"])
-app.include_router(teacher, prefix="/teacher", tags=["Teacher"])
-app.include_router(neutral, prefix="/neutral", tags=["Neutral"])
+app.include_router(main, prefix="api/users" ,tags=["Main"])
+app.include_router(student, prefix="api/users/student", tags=["Student"])
+app.include_router(teacher, prefix="api/users/teacher", tags=["Teacher"])
+app.include_router(neutral, prefix="api/users/neutral", tags=["Neutral"])
