93fbac03f9142ea5b4f37011549f895f412173a2
Iiya Zakharov <2146376@gmail.com>
2024-11-23 20:28:01 +0300

    Fix routing

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ from app.endpoints.main import main_router
 from app.endpoints.teacher_login import teacher_router
 from app.endpoints.user_login import user_router
 
-app.include_router(main_router, tags=["Main"])
-app.include_router(admin_router, prefix="/admin", tags=["Admin"])
-app.include_router(user_router, prefix="/student", tags=["Student"])
-app.include_router(teacher_router, prefix="/teacher", tags=["Teacher"])
+app.include_router(main_router, prefix="api/auth", tags=["Main"])
+app.include_router(admin_router, prefix="api/auth/admin", tags=["Admin"])
+app.include_router(user_router, prefix="api/auth/student", tags=["Student"])
+app.include_router(teacher_router, prefix="api/auth/teacher", tags=["Teacher"])
