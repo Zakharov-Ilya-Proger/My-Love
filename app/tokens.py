b5967f852e88766df1eca5cca89e1862374a995f
Iiya Zakharov <2146376@gmail.com>
2024-11-24 19:06:39 +0300

    Replase timedelta for access token

--- app/tokens.py
+++ app/tokens.py
@@ -16,9 +16,9 @@ def create_access_token(data: dict, expires_delta: timedelta):
             expire = time_now + timedelta(days=1)
     elif expires_delta == timedelta(minutes=15):
         if minutes_after_23 >= 45:
-            expire = (datetime.combine(datetime.today() + timedelta(days=1), datetime.min.time()) + expires_delta)
+            expire = (datetime.combine(datetime.today() + timedelta(days=1), datetime.min.time()) + timedelta(hours=1))
         else:
-            expire = time_now + expires_delta
+            expire = time_now + timedelta(hours=1)
     else:
         expire = time_now + expires_delta
 
